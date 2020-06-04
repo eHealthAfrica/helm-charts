f4cba4fea3830c6573e7e6848db79111ba707875
Oluwafemi Olofintuyi <oluwafemi.t.olofintuyi@gmail.com>
2020-06-04 03:38:10 -0500

    update chart

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-for CHART in `ls src`; do helm3 package --destination charts/ src/$CHART; done
-helm3 repo index . --url https://ehealthafrica.github.io/helm-charts --merge index.yaml
+for CHART in `ls src`; do helm package --destination charts/ src/$CHART; done
+helm repo index . --url https://ehealthafrica.github.io/helm-charts --merge index.yaml
