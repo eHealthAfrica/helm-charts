08341f22d07b493d89599605032c393e542b0251
Lutz Paelike <lutz.paelike@ehealthafrica.org>
2023-08-08 12:42:12 +0200

    upgrade gce_proxy to support newer database versions, deploy charts

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-
-for CHART in `ls src`; do helm package --destination charts/ src/$CHART; done
+for CHART in src/*; do helm package --destination charts/ $CHART; done
 helm repo index . --url https://ehealthafrica.github.io/helm-charts --merge index.yaml
